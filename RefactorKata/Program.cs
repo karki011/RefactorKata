ddd562901af08545159590069486af8e7b43ec1b
Subash Karki <karki011@gmail.com>
2018-01-24 09:21:14 -0800

    Finished refactoring

--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -9,27 +9,25 @@ namespace RefactorKata
         static void Main(string[] args)
         {
             // TODO add connection config when its taught 
-
-            var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
-
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = "select * from Products";
-
-            var reader = cmd.ExecuteReader();
-            var products = new List<Product>();
-
-            //TODO: Replace with Dapper once taught
-            while (reader.Read())
+            using (var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;"))
             {
-                products.Add(new Product { Name = reader["Name"].ToString() });
-            }
-            conn.Dispose();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from Products";
 
-            foreach (var product in products)
-            {
-                Console.WriteLine(product.Name);
+                var reader = cmd.ExecuteReader();
+                var products = new List<Product>();
+
+                //TODO: Replace with Dapper once taught
+                while (reader.Read())
+                {
+                    products.Add(new Product { Name = reader["Name"].ToString() });
+                }
+
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product.Name);
+                }
             }
         }
     }
-
 }
