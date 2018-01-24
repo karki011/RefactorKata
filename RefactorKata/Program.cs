3c7288bce5fd7831f61f74b987ed8e07e42f124b
Subash Karki <karki011@gmail.com>
2018-01-24 07:23:53 -0800

    Refactored with implicit keyword var

--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace RefactorKata
 {
@@ -7,16 +8,13 @@ namespace RefactorKata
     {
         static void Main(string[] args)
         {
-            //This is intentionally bad : (  Let's Refactor!
-            System.Data.SqlClient.SqlConnection Conn = new System.Data.SqlClient.SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
+            var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
 
-            System.Data.SqlClient.SqlCommand cmd = Conn.CreateCommand();
+            var cmd = conn.CreateCommand();
             cmd.CommandText = "select * from Products";
-            /*
-             * cmd.CommandText = "Select * from Invoices";
-             */
-            System.Data.SqlClient.SqlDataReader reader = cmd.ExecuteReader();
-            List<Product> products = new List<Product>();
+           
+            var reader = cmd.ExecuteReader();
+            var products = new List<Product>();
 
             //TODO: Replace with Dapper
             while (reader.Read())
@@ -25,9 +23,9 @@ namespace RefactorKata
                 prod.name = reader["Name"].ToString();
                 products.Add(prod);
             }
-            Conn.Dispose();
+            conn.Dispose();
             Console.WriteLine("Products Loaded!");
-            for (int i =0; i< products.Count; i++)
+            for(var i =0; i< products.Count; i++)
             {
                 Console.WriteLine(products[i].name);
             }
